fb178ecd739b2a7694c32388ce01af3402cf6eb9
Remi Rampin <remirampin@gmail.com>
2016-08-10 19:42:40 -0400

    Add extra files

---
+++ README.rst
@@ -0,0 +1,4 @@
+Matrix application service using Twisted
+========================================
+
+I'm not quite sure what this is going to do.
---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+
+# pip workaround
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+
+with open('README.rst') as fp:
+    description = fp.read()
+req = [
+    'Twisted']
+setup(name='matrix-gitter-twisted',
+      version='0.1',
+      packages=['matrix_gitter'],
+      entry_points={
+          'console_scripts': [
+              'matrix_gitter = matrix_gitter.main:main']},
+      install_requires=req,
+      description="Twisted-based client for Matrix",
+      author="Remi Rampin",
+      author_email='remirampin@gmail.com',
+      maintainer="Remi Rampin",
+      maintainer_email='remirampin@gmail.com',
+      long_description=description,
+      license='BSD',
+      keywords=['matrix', 'twisted'],
+      classifiers=[
+          'Development Status :: 1 - Planning',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 2.7'])
