af4234348ccc388b9addd5e46d2bcd2a60b79cf7
Remi Rampin <remirampin@gmail.com>
2016-08-10 18:07:37 -0400

    Ported Flask example to Twisted, doesn't work

--- matrix_gitter/main.py
+++ matrix_gitter/main.py
@@ -10,34 +10,90 @@ if __name__ == '__main__':
     main()
 
 
+import json
+import sys
 from twisted.internet import reactor
-from twisted.web.server import Site
-from twisted.web.resource import Resource
+from twisted.web.client import Agent
+from twisted.web.http_headers import Headers
+from twisted import logger
+from twisted.web.resource import Resource, NoResource
+from twisted.web.server import NOT_DONE_YET, Site
+
+from matrix_gitter.utils import StringProducer
 
 
-class TransactionRoot(Resource):
-    def getChild(self, name, request):
-        return Transaction(name)
+logger.globalLogPublisher.addObserver(
+    logger.FileLogObserver(sys.stderr, logger.formatEventAsClassicLogText))
+
+
+HOMESERVER_URL = 'http://10.4.0.1:8441/'
+HOMESERVER_TOKEN = 'yOBsbMzpRXQOD+7KF9yTGzlJbgxK2z+Nmq0E082C'
+
+
+log = logger.Logger()
+
+agent = Agent(reactor)
 
 
 class Transaction(Resource):
     isLeaf = True
 
-    def __init__(self, transaction):
-        Resource.__init__(self)
-        self.transaction = transaction
-
     def render_PUT(self, request):
+        if len(request.postpath) == 1:
+            transaction, = request.postpath
+        else:
+            raise NoResource
+
+        events = json.load(request.content)['events']
+        log.info("Got {nb} events", nb=len(events))
+        for event in events:
+            log.info(user=event['user_id'], room=event['room_id'])
+            log.info(event_type=event['type'])
+            log.info(content=event['content'])
         request.responseHeaders.addRawHeader(b"content-type",
                                              b"application/json")
-        return "transaction: %s\ncontent: %s\n" % (
-            self.transaction,
-            request.content.read())
+        return '{}'
+
+
+class Alias(Resource):
+    isLeaf = True
+
+    def _err(self, err):
+        log.error("Error creating room: {err}", err=str(err))
+        return None
+
+    def _end(self, request):
+        log.info("callback done")
+        request.responseHeaders.addRawHeader(b"content-type",
+                                             b"application/json")
+        request.write('{}')
+        request.finish()
+
+    def render_GET(self, request):
+        if len(request.postpath) == 1:
+            alias, = request.postpath
+        else:
+            raise NoResource
+
+        log.info("Requested room {room}", room=alias)
+        alias_localpart = alias.split(':', 1)[0][1:]
+        d = agent.request(
+            'POST',
+            '%s_matrix/client/api/v1/createRoom?access_token=%s' % (
+                HOMESERVER_URL,
+                HOMESERVER_TOKEN),
+            Headers({'content-type': ['application/json']}),
+            StringProducer(json.dumps({'room_alias_name': alias_localpart})))
+        d.addErrback(self._err)
+        d.addBoth(lambda res: self._end(request))
+        return NOT_DONE_YET
 
 
 def main():
     root = Resource()
-    root.putChild("transaction", TransactionRoot())
+    root.putChild("transaction", Transaction())
+    root.putChild("rooms", Alias())
     factory = Site(root)
-    reactor.listenTCP(4805, factory)
+    factory.logRequest = True
+    reactor.listenTCP(8445, factory)
     reactor.run()
---
+++ matrix_gitter/utils.py
@@ -0,0 +1,21 @@
+from twisted.web.iweb import IBodyProducer
+from twisted.internet import defer
+from zope.interface import implements
+
+
+class StringProducer(object):
+    implements(IBodyProducer)
+
+    def __init__(self, body):
+        self.body = body
+        self.length = len(body)
+
+    def startProducing(self, consumer):
+        consumer.write(self.body)
+        return defer.succeed(None)
+
+    def pauseProducing(self):
+        pass
+
+    def stopProducing(self):
+        pass
